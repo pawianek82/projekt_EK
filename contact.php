3477f2bda4f7ecf88e3bc019272f99424c4aeef5
pawianek82 <p.licznerski@gmail.com>
2024-12-03 00:39:06 +0100

    update styles.css and contact.php radiobutton, datei upload

--- contact.php
+++ contact.php
@@ -1,30 +1,71 @@
 <!-- Kontaktformular -->
 <section class="contact-section">
-  <h1>Kontaktieren Sie mich</h1> 
-  <form action="send_contact.php" method="post">
+  <h1>Kontaktieren Sie mich</h1>
+  <form action="send_contact.php" method="post" enctype="multipart/form-data">
     
     <!-- Eingabefeld für den Namen -->
-    <div class="form-group"> 
-      <label for="name">Ihr Name:</label> 
+    <div class="form-group">
+      <label for="name">Ihr Name:</label>
       <input
-        type="text" 
-        id="name" 
-        name="name" 
-        placeholder="Geben Sie Ihren Namen ein" 
-        required 
+        type="text"
+        id="name"
+        name="name"
+        placeholder="Geben Sie Ihren Namen ein"
+        required
       />
     </div>
 
     <!-- Eingabefeld für die Email-Adresse -->
-    <div class="form-group"> 
-      <label for="email">Ihre Email-Adresse:</label> 
-      <input type="email" id="email" name="email" placeholder="Geben Sie Ihre Email ein" required />
+    <div class="form-group">
+      <label for="email">Ihre Email-Adresse:</label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        placeholder="Geben Sie Ihre Email ein"
+        required
+      />
     </div>
 
     <!-- Eingabefeld für die Nachricht -->
-    <div class="form-group"> 
+    <div class="form-group">
       <label for="message">Ihre Nachricht:</label>
-      <textarea id="message" name="message" rows="5" placeholder="Schreiben Sie Ihre Nachricht hier" required ></textarea>
+      <textarea
+        id="message"
+        name="message"
+        rows="5"
+        placeholder="Schreiben Sie Ihre Nachricht hier"
+        required
+      ></textarea>
+    </div>
+
+    <!-- Eingabefeld für Datei-Upload -->
+    <div class="form-group">
+      <label for="file">Datei hochladen:</label>
+      <input name="file" type="file" id="file" />
+    </div>
+
+    <!-- Radio-Buttons für Geschlecht -->
+    <div class="form-group-radio">
+      <label for="male">Male</label>
+      <input
+        type="radio"
+        name="gender"
+        id="male"
+        value="male"
+        title="Falls Ihr Geschlecht männlich ist, wählen Sie diesen Knopf"
+        required
+      />
+
+      <label for="female">Female</label>
+      <input
+        type="radio"
+        name="gender"
+        id="female"
+        value="female"
+        title="Falls Ihr Geschlecht weiblich ist, wählen Sie diesen Knopf"
+        required
+      />
     </div>
 
     <!-- Senden-Button -->
