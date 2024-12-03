5369348427c29376f8b1e8bbe22790f7f5222b3a
pawianek82 <p.licznerski@gmail.com>
2024-12-03 02:38:03 +0100

    update secret.php

---
+++ secret.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Geheime Seite</title>
+    <!--Link CSS Getbootstrap.com-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!--Link CSS styles.css-->
+    <link rel="stylesheet" href="./assets/css/styles.css">
+  </head>
+
+  <body>
+    <!-- Sektion für geheime Informationen -->
+    <section class="secret-section d-flex flex-column align-items-center">
+      <div class="container text-center">
+        <h1>Geheime Informationen</h1>
+        <p>Vollständiger Name: <b>Paweł Licznerski</b></p>
+        <p>Adresse: <b>Schmiedestraße 123, 1234 Kleve</b></p>
+        <a href="index.php" class="btn btn-primary mt-3">Zurück zur Startseite</a> <!-- Flexbox-Knopf -->
+      </div>
+    </section>
+
+    <div class="image-container">
+      <p style="color: yellow; font-size: 20px;">geheim picnic</p>
+      <img src="./assets/img/geheim_picnic.jpg" alt="geheim picnic">
+      
+    </div>
+
+    <!-- Link JS Getbootstrap.com -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+  
+  <footer class="container py-3 my-4">
+    <p class="text-center text-body-secondary">2024 pawianek82@</p>
+  </footer>
+</html>
